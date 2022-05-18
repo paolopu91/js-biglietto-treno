5f5621f14d4f32346ea2fef190049d6fdb8c6454
paolopu91 <paolo.pugliese1991@gmail.com>
2022-05-18 19:50:03 +0200

    ce l'ho fatta!

--- js/main.js
+++ js/main.js
@@ -21,14 +21,22 @@ if (etaViaggiatore < 18 ){
 let costoTotale = prezzo * kmDaFare;
 costoTotale = costoTotale.toFixed(2)
 
-eta = (`${etaViaggiatore}`)
-km = (`${kmDaFare}`)
-costo = (`${costoTotale}`)
+const spanNomeUtente = document.getElementById("nome-utente")
+const spanKmViaggio = document.getElementById("km-travel")
+const spanPrezzoViaggio = document.getElementById("prezzo-totale")
 
-var eta = document.getElementById("etaViaggiatore")
-var km = document.getElementById("kmDaFare")
-var price = document.getElementById("costoTotale")
+spanNomeUtente.innerHTML = `<strong class="text-red">${etaViaggiatore} <span> età viaggiatore</span> </strong><br>`
+spanKmViaggio.innerHTML = `<strong class="text-blu">${kmDaFare} <span> km da fare</span> </strong><br>` 
+spanPrezzoViaggio.innerHTML = `<strong class="text-green">${costoTotale}<span>€ totale biglietto</span></strong>`
 
+
+// eta = (`${etaViaggiatore}`)
+// km = (`${kmDaFare}`)
+// costo = (`${costoTotale}`)
+
+// var eta = document.getElementById("etaViaggiatore")
+// var km = document.getElementById("kmDaFare")
+// var price = document.getElementById("costoTotale")
 
 // etaViaggiatore.innerHTML = "<div>Età del viaggiatore :"+eta+"</div>"
 // kmDaFare.innerHTML = "<div>Km che dovrà effettuare :"+km+"</div>"
