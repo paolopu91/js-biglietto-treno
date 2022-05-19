3047c1b7eb335222b5834c166f8a573f2a13f8f7
paolopu91 <paolo.pugliese1991@gmail.com>
2022-05-19 10:30:46 +0200

    aggiunti altri metodi con correzione Florian

--- js/bonus.js
+++ js/bonus.js
@@ -1,1 +1,3 @@
 alert("Qui di seguito dovrà mettere alcuni dati, le ricordiamo che la tratta costa 0.21 centesimi a km")
+
+
--- js/main.js
+++ js/main.js
@@ -1,26 +1,47 @@
 let kmDaFare = prompt ("Quanti km deve percorrere?");
 let etaViaggiatore = prompt ("Quale è la sua età?");
+let datiValidi = true;
 let prezzo = 0.21;
 
-console.log(kmDaFare * prezzo);
+console.log(parseInt(kmDaFare) * prezzo);
 
 console.log(kmDaFare, etaViaggiatore);
 
-if (isNaN(etaViaggiatore) && isNaN(kmDaFare)) {
+// Validare i dati che l'utente ha inserito
+
+if  (isNaN (etaViaggiatore) && isNaN (kmDaFare)) {
     alert("Inserire i valori giusti all'interno dei campi")
 }
 
-if (etaViaggiatore < 18 ){
+if (eta< 14){
+    datiValidi = false
+    alert("Sei troppo piccolo, Torna a casa!")
+}
+
+if (km < 0) {
+    datiValidi = false
+    alert("I km indicati sono inesistenti")
+}
+
+if (km > 2000){
+    datiValidi = false
+    alert("Non arriviamo fuori dall'Italia")
+}
+
+// Variabile
+
+
+if (parseInt(etaViaggiatore) < 18 ){
     let prezzo20 = prezzo * 0.20
     prezzo -= prezzo20
     console.log("il prezzo del biglietto sarà scontato del 20%")
-} else if (etaViaggiatore > 65) {
+} else if (parseInt(etaViaggiatore) > 65) {
     let prezzo40 = prezzo * 0.40
     prezzo -= prezzo40
     console.log("il prezzo del biglietto sarà scontato del 40%")
 }
 
-let costoTotale = prezzo * kmDaFare;
+let costoTotale = prezzo * parseInt(kmDaFare);
 costoTotale = costoTotale.toFixed(2)
 
 const spanNomeUtente = document.getElementById("nome-utente")
